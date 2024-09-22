cfd9104e36e4ff0a41cbd5964c87d26bf0849bb9
daniel sinteck <d.sint3ck@gmail.com>
2024-09-22 22:50:25 +0200

    ++ pageNotFound component;

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent, data: { animation: 'openClosePage' } },
-    
+    { path: '**', component: PageNotFoundComponent },
 ];
