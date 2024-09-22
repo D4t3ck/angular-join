6c8b612bbbfb573f2943dc61fb4c8a7a181299fb
daniel sinteck <d.sint3ck@gmail.com>
2024-09-22 22:58:30 +0200

    ++ common stlyes;

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent, data: { animation: 'openClosePage' } },
-    { path: '**', component: PageNotFoundComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
