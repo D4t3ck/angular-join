3f2609a24e47b81c98ed12c6f77a10bff5ef2876
daniel sinteck <d.sint3ck@gmail.com>
2024-10-01 17:17:47 +0200

    ++ signup logo & legal informations;

--- src/app/main/sign-up/sign-up.component.ts
+++ src/app/main/sign-up/sign-up.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrl: './sign-up.component.scss',
 })
 export class SignUpComponent {
+  constructor(private router: Router) {}
 
+  linkToImprint() {
+    this.router.navigate(['imprint-blank']);
+  }
+  linkToPrivacyPolicy() {
+    this.router.navigate(['privacy-policy-blank']);
+  }
 }
