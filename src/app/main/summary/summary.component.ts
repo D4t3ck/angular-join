b22c721bc3eaa45836211814df3bbf1d464cc5aa
daniel sinteck <d.sint3ck@gmail.com>
2024-10-02 14:07:17 +0200

    ++ navbar implemention;

--- src/app/main/summary/summary.component.ts
+++ src/app/main/summary/summary.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
