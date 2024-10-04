c164cec4bff75fd4e17c792f156782f8439940a7
daniel sinteck <d.sint3ck@gmail.com>
2024-10-04 17:07:46 +0200

    ++ header component html;

--- src/app/main/summary/summary.component.ts
+++ src/app/main/summary/summary.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
+import { HeaderComponent } from "../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HeaderComponent],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
