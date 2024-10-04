5c8e9ca16bb5b701c9a19dc3752c5a57c4e51de9
daniel sinteck <d.sint3ck@gmail.com>
2024-10-04 18:09:48 +0200

    ++ header impl. in components;

--- src/app/imprint & privacy policy/privacy-policy/privacy-policy.component.ts
+++ src/app/imprint & privacy policy/privacy-policy/privacy-policy.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
+import { HeaderComponent } from "../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HeaderComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
--- src/app/main/add-task/add-task.component.ts
+++ src/app/main/add-task/add-task.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
+import { HeaderComponent } from "../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HeaderComponent],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
