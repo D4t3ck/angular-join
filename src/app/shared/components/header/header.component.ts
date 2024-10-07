8fc0584b05aa419b54d91b9f50429833c4986f9e
daniel sinteck <d.sint3ck@gmail.com>
2024-10-08 00:29:41 +0200

    ++ header menu animation changed to AngularAnimations;

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  animations: [
+    trigger('slideInOut', [
+      state('hidden', style({
+        right: '-200px',
+        display: 'none'
+      })),
+      state('visible', style({
+        right: '0.5rem',
+        display: 'flex'
+      })),
+      transition('hidden => visible', [
+        style({ display: 'flex' }),
+        animate('300ms ease-in')
+      ]),
+      transition('visible => hidden', [
+        animate('300ms ease-out', style({ right: '-200px' })),
+        style({ display: 'none' })
+      ])
+    ])
+  ]
 })
 export class HeaderComponent {
   navbarVisible = false; 
