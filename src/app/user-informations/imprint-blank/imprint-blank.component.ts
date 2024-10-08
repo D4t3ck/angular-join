09dfd2db51af14aef279f54db8d159d8f9c39cdc
daniel sinteck <d.sint3ck@gmail.com>
2024-10-08 23:52:24 +0200

    ++ imprint blank content;

--- src/app/user-informations/imprint-blank/imprint-blank.component.ts
+++ src/app/user-informations/imprint-blank/imprint-blank.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-imprint-blank',
   standalone: true,
   imports: [],
   templateUrl: './imprint-blank.component.html',
-  styleUrl: './imprint-blank.component.scss'
+  styleUrl: './imprint-blank.component.scss',
 })
 export class ImprintBlankComponent {
+  constructor(private _location: Location) {}
 
+  navigateBack() {
+    this._location.back();
+  }
 }
--- src/app/user-informations/imprint/imprint.component.ts
+++ src/app/user-informations/imprint/imprint.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
-import { HeaderComponent } from "../../shared/components/header/header.component";
+import { Location } from '@angular/common';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
   imports: [NavbarComponent, HeaderComponent],
   templateUrl: './imprint.component.html',
-  styleUrl: './imprint.component.scss'
+  styleUrl: './imprint.component.scss',
 })
 export class ImprintComponent {
+  constructor(private _location: Location) {}
 
+  navigateBack() {
+    this._location.back();
+  }
 }
