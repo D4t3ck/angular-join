15def3e5db4d63bfe7469422015a3f5549db2c28
daniel sinteck <d.sint3ck@gmail.com>
2024-10-09 00:03:15 +0200

    ++ privacy-policy content;

--- src/app/user-informations/privacy-policy/privacy-policy.component.ts
+++ src/app/user-informations/privacy-policy/privacy-policy.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
-import { HeaderComponent } from "../../shared/components/header/header.component";
+import { Location } from '@angular/common';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
   imports: [NavbarComponent, HeaderComponent],
   templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss'
+  styleUrl: './privacy-policy.component.scss',
 })
 export class PrivacyPolicyComponent {
-
+  constructor(private _location: Location) {}
+  navigateBack() {
+    this._location.back();
+  }
 }
