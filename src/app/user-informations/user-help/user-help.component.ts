556072b0f354c05b6bb85fa8c4dc213d6eaef259
daniel sinteck <d.sint3ck@gmail.com>
2024-10-09 00:09:24 +0200

    ++ help component navigation;

--- src/app/user-informations/user-help/user-help.component.ts
+++ src/app/user-informations/user-help/user-help.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
-import { HeaderComponent } from "../../shared/components/header/header.component";
+import { Location } from '@angular/common';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-user-help',
   standalone: true,
   imports: [NavbarComponent, HeaderComponent],
   templateUrl: './user-help.component.html',
-  styleUrl: './user-help.component.scss'
+  styleUrl: './user-help.component.scss',
 })
 export class UserHelpComponent {
+  constructor(private _location: Location) {}
 
+  navigateBack() {
+    this._location.back();
+  }
 }
