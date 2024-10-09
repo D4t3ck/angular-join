fc0e84957e9f3fbbacb1721dfeba509921304117
daniel sinteck <d.sint3ck@gmail.com>
2024-10-09 14:55:52 +0200

    ++ navbar-blank & header-blank content;

--- src/app/shared/components/header-blank/header-blank.component.ts
+++ src/app/shared/components/header-blank/header-blank.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header-blank',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header-blank.component.html',
   styleUrl: './header-blank.component.scss'
 })
--- src/app/shared/components/navbar-blank/navbar-blank.component.ts
+++ src/app/shared/components/navbar-blank/navbar-blank.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-blank',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar-blank.component.html',
   styleUrl: './navbar-blank.component.scss'
 })
