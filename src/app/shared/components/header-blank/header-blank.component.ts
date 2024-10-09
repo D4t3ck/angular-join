9b32112751a7fad36c757fb8d7a4d0f4ef83ea04
daniel sinteck <d.sint3ck@gmail.com>
2024-10-09 14:25:35 +0200

    ++ header-blank / navbar-blank component;

---
+++ src/app/shared/components/header-blank/header-blank.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-blank',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-blank.component.html',
+  styleUrl: './header-blank.component.scss'
+})
+export class HeaderBlankComponent {
+
+}
