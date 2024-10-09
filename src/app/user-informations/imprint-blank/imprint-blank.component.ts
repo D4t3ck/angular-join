2e063575d6eba88102b040de87faf3b41d8a03bc
daniel sinteck <d.sint3ck@gmail.com>
2024-10-09 14:31:00 +0200

    ++ header-blank content;

--- src/app/user-informations/imprint-blank/imprint-blank.component.ts
+++ src/app/user-informations/imprint-blank/imprint-blank.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { HeaderBlankComponent } from "../../shared/components/header-blank/header-blank.component";
+import { NavbarBlankComponent } from "../../shared/components/navbar-blank/navbar-blank.component";
 
 @Component({
   selector: 'app-imprint-blank',
   standalone: true,
-  imports: [],
+  imports: [HeaderBlankComponent, NavbarBlankComponent],
   templateUrl: './imprint-blank.component.html',
   styleUrl: './imprint-blank.component.scss',
 })
