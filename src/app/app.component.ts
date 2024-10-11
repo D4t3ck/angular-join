491b08ae1a66ac1df7778ae34ab2d9424033dbe0
daniel sinteck <d.sint3ck@gmail.com>
2024-10-11 23:22:41 +0200

    ++ navbar & header positioning ref;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-join';
+  onLogin: boolean = false;
+
+  constructor(private router: Router) {
+    this.subRouterEvents();
+  }
+
+  subRouterEvents() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.checkPath(event.url);
+      }
+    });
+  }
+  /* ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.checkPath(event.urlAfterRedirects);
+    });
+  } */
+
+  checkPath(url: string) {
+    this.onLogin = url === '/login' || url === '/signup';
+  }
 }
